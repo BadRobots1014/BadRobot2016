7a712963d3af8909aa13976599d5522a5aeb3a13
turkey2349 <Turkey2349@gmail.com>
2016-01-11 20:30:34 -0500

    Moved the logging to the main robot class

--- src/org/usfirst/frc/team1014/robot/commands/CommandBase.java
+++ src/org/usfirst/frc/team1014/robot/commands/CommandBase.java
@@ -7,38 +7,41 @@ import org.usfirst.frc.team1014.robot.OI;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
-public abstract class CommandBase extends Command {
-    public static OI oi;
-    
-    // The subsystems on the final robot go here
-    
-    public static void init(NetworkTable table) {
-        //Final Subsystems
-    	
-    	//camera  = new AxisCamera("axis-camera.local");
-    	
-        // This MUST be here. If the OI creates Commands (which it very likely
-        // will), constructing it during the construction of CommandBase (from
-        // which commands extend), subsystems are not guaranteed to be
-        // yet. Thus, their requires() statements may grab null pointers. Bad
-        // news. Don't move it.
-        oi = new OI();
-        oi.init();
+public abstract class CommandBase extends Command
+{
+	public static OI oi;
 
-        // Show what command your subsystem is running on the SmartDashboard
-        //SmartDashboard.putData(exampleSubsystem);
-    }
+	// The subsystems on the final robot go here
 
-    public CommandBase(String name) {
-        super(name);
-    }
+	public static void init(NetworkTable table)
+	{
+		// Final Subsystems
 
-    public CommandBase() {
-        super();
-    }
-    
-    protected abstract void initialize();
-        
-    
-    public abstract String getConsoleIdentity();
+		// camera = new AxisCamera("axis-camera.local");
+
+		// This MUST be here. If the OI creates Commands (which it very likely
+		// will), constructing it during the construction of CommandBase (from
+		// which commands extend), subsystems are not guaranteed to be
+		// yet. Thus, their requires() statements may grab null pointers. Bad
+		// news. Don't move it.
+		oi = new OI();
+		OI.init();
+
+		// Show what command your subsystem is running on the SmartDashboard
+		// SmartDashboard.putData(exampleSubsystem);
+	}
+
+	public CommandBase(String name)
+	{
+		super(name);
+	}
+
+	public CommandBase()
+	{
+		super();
+	}
+
+	protected abstract void initialize();
+
+	public abstract String getConsoleIdentity();
 }
