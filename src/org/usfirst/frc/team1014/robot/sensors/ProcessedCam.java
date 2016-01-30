d20a659b28216a4829a5798a05a1ec3598e36740
subash <subashch6@gmail.com>
2016-01-30 16:11:54 -0500

    added processed cam

---
+++ src/org/usfirst/frc/team1014/robot/sensors/ProcessedCam.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team1014.robot.sensors;
+
+public class ProcessedCam 
+{
+	private double X;
+	private double Y;
+	
+	public double getX() {
+		return X;
+	}
+	public void setX(double x) {
+		X = x;
+	}
+	public double getY() {
+		return Y;
+	}
+	public void setY(double y) {
+		Y = y;
+	}
+}
