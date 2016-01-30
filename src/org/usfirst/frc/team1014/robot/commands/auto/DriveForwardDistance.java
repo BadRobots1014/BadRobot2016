18656f699a308ce6c4387c92a4070a5e69100d02
Tze Hei Tam <18tam.tzehei@gmail.com>
2016-01-30 11:19:23 -0500

    Added shoot and come back

---
+++ src/org/usfirst/frc/team1014/robot/commands/auto/DriveForwardDistance.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc.team1014.robot.commands.auto;
+
+import org.usfirst.frc.team1014.robot.commands.CommandBase;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveForwardDistance extends CommandBase{
+	
+	public double speed, distance, ultraDistance;
+
+	public DriveForwardDistance(double speed, double distance){
+		this.distance = distance;
+		this.speed = speed;
+		requires((Subsystem)driveTrain);
+	}
+	
+	@Override
+	protected void initialize() {
+		driveTrain.tankDrive(0, 0);
+		
+	}
+
+	@Override
+	public String getConsoleIdentity() {
+		return "DriveForwardDistance";
+	}
+
+	@Override
+	protected void end() {
+		driveTrain.tankDrive(0, 0);
+		
+	}
+
+	@Override
+	protected void execute() {
+		ultraDistance = driveTrain.getUltraDistance(true);
+		driveTrain.tankDrive(speed, speed);
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		System.out.println("DriveForwardDistance was interrupted");
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		if(ultraDistance < distance){
+			return true;
+		}else{
+		return false;
+		}
+	}
+
+}
