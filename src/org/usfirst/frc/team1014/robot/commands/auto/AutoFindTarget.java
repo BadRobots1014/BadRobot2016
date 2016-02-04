1cd80b4ebd34922e633f6a172e9932833245789e
Tze Hei Tam <18tam.tzehei@gmail.com>
2016-02-04 15:40:25 -0500

    Added autofindtarget

---
+++ src/org/usfirst/frc/team1014/robot/commands/auto/AutoFindTarget.java
@@ -0,0 +1,67 @@
+package org.usfirst.frc.team1014.robot.commands.auto;
+
+import org.usfirst.frc.team1014.robot.commands.CommandBase;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class AutoFindTarget extends CommandBase {
+
+	public int x, y;
+
+	public AutoFindTarget() {
+		requires((Subsystem) driveTrain);
+		requires((Subsystem) shooter);
+	}
+
+	@Override
+	protected void initialize() {
+		driveTrain.tankDrive(0, 0);
+		shooter.rotate(0);
+	}
+
+	@Override
+	public String getConsoleIdentity() {
+		return "AutoFind";
+	}
+
+	@Override
+	protected void end() {
+		driveTrain.tankDrive(0, 0);
+		shooter.rotate(0);
+
+	}
+
+	@Override
+	protected void execute() {
+
+		if (x < 0) {
+			driveTrain.tankDrive(1, -1);
+		}
+		if (x > 0) {
+			driveTrain.tankDrive(-1, 1);
+		}
+
+		if (y < 0) {
+			shooter.rotate(1);
+		}
+		if (y > 0) {
+			shooter.rotate(-1);
+		}
+
+	}
+
+	@Override
+	protected void interrupted() {
+		System.out.println("AutoFind was interrupted");
+
+	}
+
+	@Override
+	protected boolean isFinished() {
+		if ((Math.abs(x) < 5) && (Math.abs(y) < 5)) {
+			return true;
+		}
+		return false;
+	}
+
+}
