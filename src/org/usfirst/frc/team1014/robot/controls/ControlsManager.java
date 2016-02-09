5a0df98e1b6fc5f38b1d5a41b6015a2e47517319
turkey2349 <Turkey2349@gmail.com>
2016-02-09 09:12:59 -0500

    Added layouts to the controlsManager

--- src/org/usfirst/frc/team1014/robot/controls/ControlsManager.java
+++ src/org/usfirst/frc/team1014/robot/controls/ControlsManager.java
@@ -8,17 +8,17 @@ import edu.wpi.first.wpilibj.DriverStation;
  */
 public class ControlsManager
 {
-	//Button Mapping
+	// Button Mapping
 	public static final int BACK_LEFT_SPEED_CONTROLLER = 1;
 	public static final int FRONT_LEFT_SPEED_CONTROLLER = 5;
 	public static final int BACK_RIGHT_SPEED_CONTROLLER = 2;
 	public static final int FRONT_RIGHT_SPEED_CONTROLLER = 6;
-	
+
 	public static final int SHOOTER_LEFT = 3;
 	public static final int SHOOTER_RIGHT = 4;
 	public static final int SHOOTER_ROTATE = 7;
 	public static final int RING_LIGHT = 9;
-	
+
 	public static final int PUSHER = 8;
 
 	// DIO
@@ -27,12 +27,42 @@ public class ControlsManager
 	public static final int RETRO_SENSOR = 0;
 
 	public static DriverStation driverStation;
-	public static XboxController primaryXboxController, secondaryXboxController;
+	public static XboxController primaryXboxController,
+			secondaryXboxController;
 
 	public static void init()
 	{
 		driverStation = DriverStation.getInstance();
 		primaryXboxController = new XboxController(0);
 		secondaryXboxController = new XboxController(1);
+	}
+
+	/**
+	 * 
+	 * @param number
+	 *            of the controller to get the layout of (1 = primary, 2 = secondary)
+	 * @return number of the layout that should be used with the current controller ( 0 = Default,
+	 *         1&2 = Alternate layouts)
+	 */
+	public static int getLayout(int controller)
+	{
+		if(controller == 1)
+		{
+			if(primaryXboxController.isLBButtonPressed())
+				return 1;
+			else if(primaryXboxController.isRBButtonPressed())
+				return 2;
+			else
+				return 0;
+		}
+		else
+		{
+			if(secondaryXboxController.isLBButtonPressed())
+				return 1;
+			else if(secondaryXboxController.isRBButtonPressed())
+				return 2;
+			else
+				return 0;
+		}
 	}
 }
