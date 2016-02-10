187a53d5feba4fbcc032607c3e5c724435372057
subash <subashch6@gmail.com>
2016-02-09 20:49:44 -0500

    got rid of the arduino code

---
+++ src/org/usfirst/frc/team1014/robot/utilities/SmartDashboard.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1014.robot.utilities;
+
+
+public class SmartDashboard
+{
+	public static void initDashboard()
+	{
+//		CameraServer server = CameraServer.getInstance();
+//		server.startAutomaticCapture("cam0");
+//		Logger.log(Logger.Level.Debug, "SmartDash", "Camera initialized");
+	}
+}
