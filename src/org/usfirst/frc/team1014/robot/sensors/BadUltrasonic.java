b3579e30a07f79e8108cca4039371b768267d0b7
singhalmanu9 <singhalmanu9@gmail.com>
2016-02-10 10:42:12 -0500

    recalibrated the sensor?

---
+++ src/org/usfirst/frc/team1014/robot/sensors/BadUltrasonic.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team1014.robot.sensors;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+
+/**
+ * This class is the wrapper class for the MaxBotix ultrasonic
+ * range finder. It has a maximum range of around 64 inches.
+ * @author Manu S. 
+ *
+ */
+public class BadUltrasonic extends AnalogInput
+{
+	private static double SCALE_FACTOR = 0.6005858778953552 / 64; // in V/inches
+	
+	public BadUltrasonic(int ultraChannel)
+	{
+		super(ultraChannel);
+	}
+	
+	public double getDistance()
+	{
+		return ((AnalogInput) this).getVoltage() / SCALE_FACTOR;
+		
+	}
+}
