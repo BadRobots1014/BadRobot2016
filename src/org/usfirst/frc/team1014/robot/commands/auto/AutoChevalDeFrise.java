b38664cd2f28f15f4ed687257f9fc2ecae110850
Tze Hei Tam <18tam.tzehei@gmail.com>
2016-02-13 14:38:40 -0500

    Added cheval de frise code

---
+++ src/org/usfirst/frc/team1014/robot/commands/auto/AutoChevalDeFrise.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team1014.robot.commands.auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutoChevalDeFrise extends CommandGroup{
+
+	public AutoChevalDeFrise()
+	{
+		this.addSequential(new AutoRotateDown());
+		this.addSequential(new AutoDrive(2, 1));
+		this.addSequential(new AutoRotate(.5, true));
+	}
+	
+}
